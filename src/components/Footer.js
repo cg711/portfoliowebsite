e44c4e372f00de962a62a030eec2498235d62e8a
cg711 <caseyg08@icloud.com>
2023-06-05 21:11:50 -0500

    Removed cringe footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return(
         <div className="w-full h-36 bg-gray-300 flex justify-center items-center">
-            <h1>Made with ❤️</h1>
+            {/* <h1>Made with ❤️</h1> */}
         </div>
     );
 }
